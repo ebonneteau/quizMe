fe451d99b495d78421d02a78c8f1fa24e3ca394e
rico <rico@rico-pc>
2019-12-06 16:00:59 +0100

    Creation of Class user to get user name input

--- app/src/main/java/com/eric/quizme/controller/MainActivity.java
+++ app/src/main/java/com/eric/quizme/controller/MainActivity.java
@@ -12,14 +12,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.eric.quizme.R;
+import com.eric.quizme.model.User;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mGreetingText;
     private EditText mNameInput;
     private Button mPlayButton;
+    private User mUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mUser = new User();
         setContentView(R.layout.activity_main);
         mGreetingText = findViewById(R.id.activity_main_greeting_txt);
         mNameInput = findViewById(R.id.activity_main_name_input);
@@ -44,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+            String firstname = mNameInput.getText().toString();
+            mUser.setFirstname(firstname);
             Intent gameActivity = new Intent(MainActivity.this, GameActivity.class);
             startActivity(gameActivity);
             }
---
+++ app/src/main/java/com/eric/quizme/model/Questions.java
@@ -0,0 +1,10 @@
+package com.eric.quizme.model;
+
+import java.util.List;
+
+public class Questions {
+    private String mQuestion;
+    private List<String> mChoiceList;
+    private int mAnswerIndex;
+    
+}
