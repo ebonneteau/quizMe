fed97f423567228f79883918f8586723f8332efc
rico <rico@rico-pc>
2019-12-06 17:28:02 +0100

    New branch for test to implement Question class Method by myself...Not won..

--- app/src/main/java/com/eric/quizme/model/Questions.java
+++ app/src/main/java/com/eric/quizme/model/Questions.java
@@ -1,10 +1,37 @@
 package com.eric.quizme.model;
 
 import java.util.List;
+import java.util.Random;
 
 public class Questions {
     private String mQuestion;
     private List<String> mChoiceList;
     private int mAnswerIndex;
-    
+
+    public String getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(String question) {
+        mQuestion = question;
+    }
+
+    public List<String> getChoiceList() {
+        if (getChoiceList()==null) {
+            getQuestion();
+        }
+        return mChoiceList;
+    }
+
+    public void setChoiceList(List<String> choiceList) {
+        mChoiceList = choiceList;
+    }
+
+    public int getAnswerIndex() {
+        return mAnswerIndex;
+    }
+
+    public void setAnswerIndex(int answerIndex) {
+        mAnswerIndex = answerIndex;
+    }
 }
